c3dafe3e8f47cb6008373a0c5d7a95fa57f9ea74
Gabriel-Arj <arjbiell@gmail.com>
2026-06-22 18:25:37 -0300

    Atualizar o nme por indice.py

--- nme por indice.py
+++ nme por indice.py
@@ -1,12 +1,9 @@
 def num_nom():
-    print("Ohayo " + nome[nom_num])
-def drgn():
-    drgn_inpt = input("O que você achou GPT?")
-nome = ["Gabriel", "Talita", "Edivaldo", "Graça"]
+    print("Olá " + nome[nom_num])
+nome = ["Gabriel", "João", "Helena", "Maria"]
 nom_num = int(input("Escolha um numero de 0 a 3: "))
 while nom_num not in range(len(nome)):
-    print("Esse numero não esta corrento, tente novamente")
+    print("Esse numero não esta correto, tente novamente")
     nom_num = int(input("Escolha um numero de 0 a 3: "))
 
 num_nom()
-drgn()
